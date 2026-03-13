f15366c4de8a01d54a473980421d37f1abf7f50b
Thamaraiselvan21ai <tamaraiselvan2003@gmail.com>
2026-03-13 18:51:46 +0530

    Changes done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Thamaraiselvan V",
+    author_email="tamaraiselvan2003@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
