066501dbb6ad183e87017d20e7a20624e9b51b84
Thamaraiselvan21ai <tamaraiselvan2003@gmail.com>
2026-03-16 11:55:23 +0530

    App

--- app.py
+++ app.py
@@ -98,5 +98,9 @@ def chat():
 
 
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port= 8080, debug= True)
+# if __name__ == '__main__':
+#     app.run(host="0.0.0.0", port= 10000, debug= True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
