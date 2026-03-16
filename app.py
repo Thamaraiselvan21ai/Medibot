9b1b47b17d645d4e5cea11436b563b4ef2530042
Thamaraiselvan21ai <tamaraiselvan2003@gmail.com>
2026-03-16 10:58:22 +0530

    App

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ import os
 
 llm = ChatGroq(
     model="llama-3.3-70b-versatile",
-    groq_api_key=os.getenv("GROQ_API_KEY"),
+    #groq_api_key=os.getenv("GROQ_API_KEY"),
     temperature=0
 )
 
